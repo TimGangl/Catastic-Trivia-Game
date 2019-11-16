7e8196a0a07dd1789805bf057076f54b3638d6d0
Tim Gangl <tim.gangl30@gmail.com>
2019-11-15 16:48:37 -0800

    game rework and finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,146 +1,173 @@
-$(document).ready(function () {
-    //questions and answers object------------------------------------------
-    var questions = [{
-        ques: "1) What is the proper term for a group of kittens?",
-        ans: ["Kaboodle", "Kindle", "Kine", "Kettle"],
-        correct: "Kindle"
-    },
-    {
-        ques: "2) All cats are born with what color eyes?",
-        ans: ["Green", "Blue", "Black", "Brown"],
-        correct: "Blue eyes"
-    },
-    {
-        ques: "3) How many different sounds can a cat make?",
-        ans: ["10", "27", "150", "100"],
-        correct: "100"
-    },
-    {
-        ques: "4) how many breeds of domestic cat are there worldwide?",
-        ans: ["70", "140", "280", "210"],
-        correct: "70"
-    },
-    {
-        ques: "5) What is the normal body tempature of a cat?",
-        ans: ["102°F", "94°F", "106°F", "98°F"],
-        correct: "102°F"
-    },
-    {
-        ques: "6) What breed of cat has a reputation for being cross-eyed?",
-        ans: ["Egyptian Mau", "Himalayan", "Persian", "Siamese"],
-        correct: "Siamese"
-    },
-    {
-        ques: "7) Which country has more cats per person than any other country in the World?",
-        ans: ["Denmark", "USA", "India", "New Zealand"],
-        correct: "New Zealand"
-    },
-    {
-        ques: "8) What percent of people identify as  cat people?",
-        ans: ["34.6%", "48.2%", "27.9%", "11.5%"],
-        correct: "11.5%"
-    },
-    {
-        ques: "9) How fast can cats run?",
-        ans: ["40mph", "50mph", "20mph", "30mph"],
-        correct: "30mph"
-    },
-    {
-        ques: "10) Why do cats rub against you?",
-        ans: ["To show affection", "To mark you with its sent", "To say hello", "All of these"],
-        correct: "All of these"
-    }];
+var questions = [{
+  ques: "10) What is the proper term for a group of kittens?",
+  ans: ["Kaboodle", "Kindle", "Kine", "Kettle"],
+  name: "kittenName",
+  correct: "Kindle",
+  divClass: ".kittenName"
+},
+{
+  ques: "9) All cats are born with what color eyes?",
+  ans: ["Green", "Blue", "Black", "Brown"],
+  name: "catEyes",
+  correct: "Blue",
+  divClass: ".catEyes"
+},
+{
+  ques: "8) How many different sounds can a cat make?",
+  ans: ["10", "27", "150", "100"],
+  name: "catSounds",
+  correct: "100",
+  divClass: ".catSounds"
+},
+{
+  ques: "7) How many breeds of domestic cat are there worldwide?",
+  ans: ["70", "140", "280", "210"],
+  name: "catBreeds",
+  correct: "70",
+  divClass: ".catBreeds"
+},
+{
+  ques: "6) What is the normal body tempature of a cat?",
+  ans: ["102°F", "94°F", "106°F", "98°F"],
+  name: "catTemp",
+  correct: "102°F",
+  divClass: ".catTemp"
+},
+{
+  ques: "5) What breed of cat has a reputation for being cross-eyed?",
+  ans: ["Egyptian Mau", "Himalayan", "Persian", "Siamese"],
+  name: "crossEye",
+  correct: "Siamses",
+  divClass: ".crossEye"
+},
+{
+  ques: "4) Which country has more cats per person than any other country?",
+  ans: ["Denmark", "USA", "India", "New Zealand"],
+  name: "catCountry",
+  correct: "New Zealand",
+  divClass: ".catCountry"
+},
+{
+  ques: "3) What percent of people identify as cat people?",
+  ans: ["34.6%", "48.2%", "27.9%", "11.5%"],
+  name: "catPeople",
+  correct: "11.5%",
+  divClass: ".catPeople"
+},
+{
+  ques: "2) How fast can cats run?",
+  ans: ["40mph", "50mph", "20mph", "30mph"],
+  name: "catSpeed",
+  correct: "30mph",
+  divClass: ".catSpeed"
+},
+{
+  ques: "1) Why do cats rub against you?",
+  ans: ["To show affection", "To mark you with its scent", "To say hello", "All of these"],
+  name: "catRub",
+  correct: "All of these",
+  divClass: ".catRub"
+}
+] // end questions object
 
-    //variables---------------------------------------------------------
-    var card = $(".card");
-    var questionDisplay = $("#ques-disp");
-    var timesUpDisplay = $("#times-up-disp");
-    var gameOverDisplay = $("#game-over");
-    var correctAnswer = $("#correct-answer");
-    var incorrectAnswer = $("#incorrect-answer");
-    var correctAns = $("#correct-ans");
-    var wrongAns = $("#wrong-ans");
-    var startBtn = $("#start-btn");
-    var answer1 = $("#ans1");
-    var answer2 = $("#ans2");
-    var answer3 = $("#ans3");
-    var answer4 = $("#ans4");
-    var i = 0;
+var labels = ["first", "second", "third", "forth"];
 
-
-    //functions---------------------------------------------------------
-
-    //start screen load hide all divs
-    function gameStart() {
-        card.hide();
-        questionDisplay.hide();
-        timesUpDisplay.hide();
-        gameOverDisplay.hide();
-        correctAnswer.hide();
-        incorrectAnswer.hide();
-        correctAns.hide();
-        wrongAns.hide();
-    };
-    //show question card
-    function showQues() {
-        questionDisplay.text(questions[i].ques);
-        answer1.text(questions[i].ans[0]);
-        answer2.text(questions[i].ans[1]);
-        answer3.text(questions[i].ans[2]);
-        answer4.text(questions[i].ans[3]);
-
-    };
-    gameStart();
-
-
-
-
-    //set timer for 5 sec that hides the card, displays times up for 3 sec, increments loss, and sets up next question (display q and a)
-
-    // when answer is clicked, clear interval timer, hide card compare against the correct answer
-
-    //if correct, clear interval timer, increment correct, diplay correct screen for 3 secs, set up next question
-
-    //if wrong, clear interval timer, increment wrong, display incorrect screen for 3 sec, set up next question
-
-    //if time runs out on question, go to times up screen, increment incorrect answer, set up next question 
-
-    //after final question diplay game over screen along with results of game
-
-    //game--------------------------------------------------------------
-
-    //on start click hide all div and show first question div
-    startBtn.on("click", function () {
-        i = 0;
-        showQues();
-        card.show();
-        questionDisplay.show();
-        $(".jumbotron").hide();
-        answer1.show();
-        answer2.show();
-        answer3.show();
-        answer4.show();
-    });
-
-
-    //start timer
-    function quesTime() {
-        setTimeout(function () {
-            card.hide();
-            timesUpDisplay.show();
-            wrongAns++;
-        });
-    }
-    quesTime();
-
-    for (i = 0; i < questions.length; i++) {
-        if (questions[i].ques === questions[i].ans) {
-            correctAnswer++;
-            correctAns.text;
-        } else incorrectAnswer++;
-        wrongAns.text;
-        showQues();
-
-    }
+// click to start then display quesions
+var startGame = $("#start-btn").on('click', function () {
+  $(this).parent().hide();
+  $('.container').show();
+  countdown(60);
+  questionDisplay();
+  var catSound = new Audio("./assets/images/kitty-meow.mp3");
+  catSound.play();
 });
 
+// function for displaying questions
+var questionDisplay = function () {
+  $(".questions :not('#sub-but')").empty();
+  // loops through the 10 questions 
+  for (var j = 0; j < 10; j++) {
+    $('.questions').prepend('<div class="' + questions[j].name + '"></div>');
+    $(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
+    // loops through answers for each radio button
+    for (var i = 0; i <= 3; i++) {
+      $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+    }
+    $('.questions').prepend('<hr />');
+  }
+}
+
+
+// function for countdown timer
+var countdown = function (seconds) {
+
+  var timer = setInterval(function () {
+    seconds = seconds - 1;
+    $("#time-remain").html(seconds);
+
+    if (seconds <= 0) {
+      $('.container').fadeOut(500);
+      var correctAnswers = 0;
+      var wrongAnswers = 0;
+      var unAnswered = 0;
+
+      // loop through correctArray & radioName to match html elements & answers
+      for (var i = 0; i < 10; i++) {
+
+        if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+          correctAnswers++;
+          console.log("this is correct! number:" + i)
+        } else {
+          wrongAnswers++;
+          console.log("this is wrong! number:" + i)
+        };
+      }
+      $('#correctTimesUp').append(correctAnswers);
+      // display wrongAnswers
+      $('#wrongTimesUp').append(wrongAnswers);
+      $('#timesUp').fadeIn(1000).show();
+
+      // alert("Times Up!");
+      clearInterval(timer);
+      return;
+    }
+  }, 1000);
+
+  // click event for submit button to stop timer
+  $('#sub-but').on('click', function () {
+    clearInterval(timer);
+  })
+};
+
+
+// function to grade quiz once submit button is clicked
+var gradeQuiz = $('#sub-but').on('click', function () {
+
+  var correctAnswers = 0;
+  var wrongAnswers = 0;
+  var unAnswered = 0;
+
+  // loop through correctArray & radioName to match html elements & answers
+  for (var i = 0; i < 10; i++) {
+
+    if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+      correctAnswers++;
+    } else {
+      wrongAnswers++;
+    };
+  };
+
+
+  countdown();
+
+  $('.container').fadeOut(500);
+
+  $('#answerScreen').show();
+
+  $('#correctScreen').append(correctAnswers);
+
+  $('#wrongScreen').append(wrongAnswers);
+
+}); 
